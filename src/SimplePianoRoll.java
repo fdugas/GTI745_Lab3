ac80b4ad5470c2fa73da642ae3090c062e6a628a
Pierre-Antoine Laine <pierre-antoine.laine.1@ens.etsmtl.ca>
2016-07-24 13:13:32 -0400

    Ajout de librairies

--- src/SimplePianoRoll.java
+++ src/SimplePianoRoll.java
@@ -1,40 +1,36 @@
 
-import java.util.ArrayList;
-
-import java.awt.Container;
-import java.awt.Component;
-import java.awt.Graphics;
 // import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
-
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JRadioButton;
-import javax.swing.JCheckBox;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.ButtonGroup;
-import javax.swing.BoxLayout;
-import javax.swing.Box;
-import javax.swing.BorderFactory;
-import javax.swing.JLabel;
-
+import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Synthesizer;
-import javax.sound.midi.MidiChannel;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
 
 
 
@@ -68,7 +64,7 @@ class Score {
 
 	public int numPitches = 88;
 	public static final int pitchClassOfLowestPitch = 9; // 9==A==la
-	public static final int midiNoteNumberOfLowestPitch = 21;
+	public final int midiNoteNumberOfLowestPitch = 21;
 	public int numBeats = 128;
 	public boolean [][] grid;
 
@@ -373,7 +369,6 @@ class MyCanvas extends JPanel implements KeyListener, MouseListener, MouseMotion
 			}
 			
 		}
-		
 		
 	}
 
